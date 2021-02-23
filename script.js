1756899bd07fd5eb317d2f84161d95129be84b39
studzin-sky <the.stucc@gmail.com>
2021-02-23 18:19:50 +0100

    updating the code to finish the excersise

--- script.js
+++ script.js
@@ -21,37 +21,44 @@ function gameScore(result) {
         browserOutcome.innerHTML += '<li>0</li>';
     }
 }
-//declare a function for a round with a browser based on a players choice
+//declare a function for a round with a browser based on a players choice, it returns two values in an array
 function playRound(choice) {
+    let point = 0;
     let computerSelection = computerPlay();
     if (choice == 'rock' & computerSelection === 'Paper'){
         result = 'You lose! Paper beats Rock!';
+        point -= 1;
     }
     else if (choice == 'rock' & computerSelection ==='Scissors'){
         result = 'You win! Rock beats Scissors!';
+        point += 1;
     }
     else if (choice == 'rock' & computerSelection ==='Rock'){
         result = 'It is a draw!';
     }
-    else if (choice == 'scissors' & computerSelection ==='rock'){
+    else if (choice == 'scissors' & computerSelection ==='Rock'){
         result = 'You lose! Rock beats Scissors!';
+        point -= 1;
     }
     else if (choice == 'scissors' & computerSelection ==='Paper'){
         result = 'You win! Scissors beats Paper!';
+        point += 1;
     }
     else if (choice == 'scissors' & computerSelection ==='Scissors'){
         result = 'It is a draw!';
     }
     else if (choice == 'paper' & computerSelection ==='Scissors'){
         result = 'You lose! Scissors beats Paper!';
+        point -= 1;
     }
     else if (choice == 'paper' & computerSelection ==='Rock'){
         result = 'You win! Paper beats Rock!';
+        point += 1;
     }
     else if (choice == 'paper' & computerSelection ==='Paper'){
         result = 'It is a draw!';
     }
-return result;
+return [result, point];
 }
 
 
@@ -59,12 +66,15 @@ return result;
 let playerSelection = button.forEach(e => {         
     e.addEventListener('click', () => {   
         let roundResult = playRound(e.id);  //result of a round based on argument id
-        let scorePrint = gameScore(roundResult); // function that adds score to the leaderboard
-        roundOutcome.innerText = roundResult;   // result of one round is shown
-        return roundResult;
+        let scorePrint = gameScore(roundResult[0]); // function that adds score to the leaderboard from index 0
+        roundOutcome.innerText = roundResult[0];   // result of one round is shown from functions index 0
+        let gameWin = gameResult;
+        gameWin.innerText += roundResult[1];
+        console.log(gameWin);
     });
 });
 
 const playOutcome = document.getElementById('playOutcome');
 const roundOutcome = document.getElementById('roundOutcome');
+const gameResult = document.getElementById('gameResult');
 const browserOutcome = document.getElementById('browserOutcome');
